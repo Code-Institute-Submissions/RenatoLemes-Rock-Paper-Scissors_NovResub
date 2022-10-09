2259f9fd0538691f37c90d2815dd9e3110c070ff
RenatoLemes <renato.lemmes@hotmail.com>
2022-10-09 20:16:31 +0000

    debuging javaScript

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,14 @@
 var playerScore = 0
 var computerScore = 0
-
 var imgPlayer = document.getElementById("player")
 var imgComputer = document.getElementById("computer")
 var playing = document.getElementById("playing") //this is the button play in the end of the game
 var score = document.getElementById("scoreGame") //score of the game
 var winner = document.getElementById("winner") 
 var loser = document.getElementById("loser") 
-
 var rock = document.getElementById("rockInput")
 var paper = document.getElementById("paperInput")
 var scissors = document.getElementById("scissorsInput")
-
 var player = ""
 var computer = ""
 
@@ -37,9 +34,9 @@ function selectGame(button) {
 }
 
 function playComputer() {
-    let opt = ['rock', 'paper', 'scissors']
-    let num = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-    computer = opt[num]
+    let options = ['rock', 'paper', 'scissors']
+    let number = Math.floor(Math.random() * 2);
+    computer = options[number]
     imgComputer.innerHTML = "<img src='assets/images/" + computer + ".svg'>"
     gameRule()
 }
@@ -55,11 +52,11 @@ function gameRule() {
     if (player == computer) {
 
     } else if (player == "rock") {
-        win = computer == 'scissors' ? 1 : -1
-    } else if (player == 'paper') {
-        win = computer == 'rock' ? 1 : -1
-    } else if (player == 'scissors') {
-        win = computer == 'paper' ? 1 : -1
+        win = computer == "scissors" ? 1 : -1
+    } else if (player == "paper") {
+        win = computer == "rock" ? 1 : -1
+    } else if (player == "scissors") {
+        win = computer == "paper" ? 1 : -1
     }
 
     if(win == 0){
@@ -95,10 +92,6 @@ function clear(){
     imgPlayer.innerHTML = ""
 }
 
-document.getElementById("again").addEventListener("click", () => {
-    newGame()
-})
-
 function restart(){
     score.innerHTML = "0 0"
     computerScore = 0
@@ -109,3 +102,4 @@ function restart(){
     loser.classList.add('none')
     loser.classList.remove('center')
 }
+
