50337ad84560a3ac4c13b7f624394047fabefa50
RenatoLemes <renato.lemmes@hotmail.com>
2022-10-10 06:43:53 +0000

    add information to README and tiding up code

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
-#Rock Paper Scissors Game
+# Rock Paper Scissors Game
+
+This is Rock Papaer Scissors game which 
 
 ![This is an image](/assets/images/AmIresponsive.jpg)
 
-...
+## Features
+
+
+### Validator Testing
+
+- #### HTML
+No errors were found. Tool used to validate: [W3C validator](https://validator.w3.org/nu/)
+
+- #### CSS
+No errors were found. Tool used to validate:: [(Jigsaw) Validator](https://jigsaw.w3.org/css-validator/)
+
+- #### JavaScript
+No errors were found. Tool used to validate: [Jshint validator](https://jshint.com/)
+    This was the Metrics of the code:
+
+```
+- There are 8 functions in this file.
+- Function with the largest signature take 1 arguments, while the median is 0.
+- Largest function has 15 statements in it, while the median is 2.5.
+- The most complex function has a cyclomatic complexity value of 12 while the median is 1.
+```
+
+### Unfixed Bugs
+
+There are no Bugs left to be fixed.
+
+## Deployement
+
+The project was deployed to GitHub Pages using the following steps:
+
+- Log in to GitHub and locate the [Rock Paper Scissor](https://github.com/RenatoLemes/Rock-Paper-Scissors) repository.
+- Go to "Settings" Button on the menu.
+- On settings in the left side of the page click on "Pages"
+- click on "Select branch:" it will drop down. Select "Main", make sure the "Select folder" has "/root" selected.
+- The page will automatically refresh and you receive message "Your site is published at https://renatolemes.github.io/Rock-Paper-Scissors/.
+
+## Credits
+
+[Love Maths Project](https://renatolemes.github.io/love-maths/) by Code Institute as a general resource.
+
+W3School as a general resource.
 
 
 color : https://color.adobe.com/search?q=1B262C&t=hex
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,91 +1,90 @@
-var imgPlayer = document.getElementById("player")
-var imgComputer = document.getElementById("computer")
-var playing = document.getElementById("playing") //this is the button play in the end of the game
-var score = document.getElementById("scoreGame") //score of the game
-var winner = document.getElementById("winner") 
-var loser = document.getElementById("loser") 
-var rock = document.getElementById("rockInput")
-var paper = document.getElementById("paperInput")
-var scissors = document.getElementById("scissorsInput")
-var player = ""
-var computer = ""
-var playerScore = 0
-var computerScore = 0
+var imgPlayer = document.getElementById("player");
+var imgComputer = document.getElementById("computer");
+var score = document.getElementById("scoreGame");
+var winner = document.getElementById("winner") ;
+var loser = document.getElementById("loser"); 
+var rock = document.getElementById("rockInput");
+var paper = document.getElementById("paperInput");
+var scissors = document.getElementById("scissorsInput");
+var player = "";
+var computer = "";
+var playerScore = 0;
+var computerScore = 0;
 
-rock.addEventListener("click", () => {
-    selectGame(rock)
-    playComputer()
-})
+rock.addEventListener("click", function() {
+    selectGame(rock);
+    playComputer();
+});
 
-paper.addEventListener("click", () => {
-    selectGame(paper)
-    playComputer()
-})
+paper.addEventListener("click", function() {
+    selectGame(paper);
+    playComputer();
+});
 
-scissors.addEventListener("click", () => {
-    selectGame(scissors)
-    playComputer()
-})
+scissors.addEventListener("click", function() {
+    selectGame(scissors);
+    playComputer();
+});
 
 function selectGame(button) {
-    player = button.value
-    imgPlayer.innerHTML = "<img src='assets/images/" + player + ".svg'>"
-    imgComputer.innerHTML = ""
+    player = button.value;
+    imgPlayer.innerHTML = "<img src='assets/images/" + player + ".svg'>";
+    imgComputer.innerHTML = "";
 }
 
 function playComputer() {
-    let options = ['rock', 'paper', 'scissors']
-    let number = Math.floor(Math.random() * 2);
-    computer = options[number]
-    imgComputer.innerHTML = "<img src='assets/images/" + computer + ".svg'>"
-    gameRule()
+    var options = ['rock', 'paper', 'scissors'];
+    var number = Math.floor(Math.random() * 2);
+    computer = options[number];
+    imgComputer.innerHTML = "<img src='assets/images/" + computer + ".svg'>";
+    gameRule();
 }
 
 function gameRule() {
-    let win = "0"
+    var win = "0";
 
     if (player == computer) {
     } else if (player == "rock") {
-        win = computer == "scissors" ? 1 : -1
+        win = computer == "scissors" ? 1 : -1;
     } else if (player == "paper") {
-        win = computer == "rock" ? 1 : -1
+        win = computer == "rock" ? 1 : -1;
     } else if (player == "scissors") {
-        win = computer == "paper" ? 1 : -1
+        win = computer == "paper" ? 1 : -1;
     }
 
     if(win == 0){
     } else if(win > 0){ 
-        playerScore = playerScore + 1
+        playerScore = playerScore + 1;
     } else {
-        computerScore = computerScore + 1
+        computerScore = computerScore + 1;
     }
 
-    score.innerHTML = playerScore + " x " + computerScore
+    score.innerHTML = playerScore + " x " + computerScore;
 
     if (playerScore >= 2){
-        winner.classList.remove('front-screen')
-        winner.classList.add('newWindow')
+        winner.classList.remove('front-screen');
+        winner.classList.add('newWindow');
     }
 
     if (computerScore >= 2){
-        loser.classList.remove('front-screen')
-        loser.classList.add('newWindow')
+        loser.classList.remove('front-screen');
+        loser.classList.add('newWindow');
     }
 }
 
 function cleanScore(){
-    imgComputer.innerHTML=""
-    imgPlayer.innerHTML = ""
+    imgComputer.innerHTML="";
+    imgPlayer.innerHTML = "";
 }
 
 function restart(){
-    cleanScore()
-    score.innerHTML = "0x0"
-    computerScore = 0
-    playerScore = 0
-    winner.classList.add('front-screen')
-    winner.classList.remove('newWindow')
-    loser.classList.add('front-screen')
-    loser.classList.remove('newWindow')
+    cleanScore();
+    score.innerHTML = "0x0";
+    computerScore = 0;
+    playerScore = 0;
+    winner.classList.add('front-screen');
+    winner.classList.remove('newWindow');
+    loser.classList.add('front-screen');
+    loser.classList.remove('newWindow');
 }
 
